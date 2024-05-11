306790b55aedf032c8fd975d6be6db48eb1cf358
Daniel Król <danielkrol90@gmail.com>
2024-05-11 10:31:17 +0200

    init react project folder

---
+++ frontend-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        It will be something here
+      </header>
+    </div>
+  );
+}
+
+export default App;
