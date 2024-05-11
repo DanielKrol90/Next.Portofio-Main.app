8d7441b1b9139b3a50287c801edaf1a8df54c7bb
Daniel Król <danielkrol90@gmail.com>
2024-05-11 16:22:33 +0200

    working on navbar styling

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 import { images } from '../../constants'
 
+import './Navbar.css'
+
 const Navbar = () => {
   return (
     <nav className='app__navbar'>
-      <div className='app_navbar-logo'>
+      <div className='app__navbar-logo'>
         <img src={images.logo} alt="logo"/>
       </div>
       <ul className='app__navbar-links'>
-        {['Strona Głóna', 'O Mnie', 'Praca', 'Umiejętności', 'Kontakt' ].map((item) => (
+        {['Strona Główna', 'O Mnie', 'Praca', 'Umiejętności', 'Kontakt' ].map((item) => (
           <li className='app__flex p-text' key={`link-${item}`}>
             <div>
               <a href={`#${item}`}>{item}</a>
