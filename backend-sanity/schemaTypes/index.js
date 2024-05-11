6a6b1ee5a83944d98bb4794eb6e9c9d92ee08539
Daniel Król <danielkrol90@gmail.com>
2024-05-11 09:20:37 +0200

    adding basic shemas needed to work

--- backend-sanity/schemaTypes/index.js
+++ backend-sanity/schemaTypes/index.js
@@ -1,1 +1,20 @@
-export const schemaTypes = []
+import abouts from "./abouts";
+import brands from "./brands"
+import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
+import workExperiences from "./workExperience"
+import works from "./works"
+
+import testimonials from "./testimonials";
+
+export const schemaTypes = [
+    abouts,
+    brands,
+    contact,
+    experiences,
+    skills,
+    testimonials,
+    workExperiences,
+    works,
+]
