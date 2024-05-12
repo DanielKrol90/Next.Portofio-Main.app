6d911b95b7d53030eba0d68fe0a857779e3ab8f9
Daniel Król <danielkrol90@gmail.com>
2024-05-12 18:31:21 +0200

    start of work on about section, creating basic structure of section

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { motion } from 'framer-motion'
+
+import './About.css'
+
+const abouts = [
+  {title: 'Rozwój strony internetowej', description: 'Rozwój obecnie posiadanej strony internetowej', imgUrl: ''},
+  {title: 'Tworzenie od zera, nowej strony internetowej', description: 'Zgodnie z pomysłem i oczekiwaniem klienta, tworzę od zera nową stronę internetową', imgUrl: ''},
+  {title: 'Poprawki i rozwój strony internetowej', description: 'Rozwój,ulepszenie oraz wprowadzenie poprawek do istniejącej strony internetowej', imgUrl: ''},
+]
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className="head-text">
+      Dobrze stworzona
+      <span >Strona internetowa Firmy</span>
+      <br/>
+      oznacza
+      <span >lepszą sprzedaż produkt oraz więcej klientów</span>
+    </h2>
+
+    <div className="app_profiles">
+      {abouts.maps((about, index) => (
+        <motion.div
+        whileInView={{ opacity: 1 }}
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.5, type: 'tween'}}
+        className='app__profile-item'
+        key={about.title + index}
+        >
+          <img src={about.imgIrl} alt={about.title}/>
+          <h2 className="bolt-text" style={{ marginTop: 20 }}>{about.title}</h2>
+          <p className="p-text"></p>
+
+
+        </motion.div>
+      ))}
+    </div>
+
+    </>
   )
 }
 
